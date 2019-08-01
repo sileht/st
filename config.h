9342542ce43a6e89e915ad2824fe329c66e69b3b
Mehdi Abaakouk <sileht@sileht.net>
2019-08-01 19:21:00 +0200

    Revert "White"
    This reverts commit 133729a9a75285db233ef8b94e234cdb4fe3a40c.

--- config.h
+++ config.h
@@ -123,39 +123,22 @@ static const char *colorname[] = {
 //	"#eeeeec",  /* 15: brwhite  */
 
     /* gruvbox */
-//	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-//	"#cc241d",
-//	"#98971a",
-//	"#d79921",
-//	"#458588",
-//	"#b16286",
-//	"#689d6a",
-//	"#a89984",
-//	"#928374",
-//	"#fb4934",
-//	"#b8bb26",
-//	"#fabd2f",
-//	"#83a598",
-//	"#d3869b",
-//	"#8ec07c",
-//	"#ebdbb2",
-
-  "#fbf1c7", /* hard contrast: #f9f5d7 / soft contrast: #f2e5bc */
+	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
 	"#cc241d",
 	"#98971a",
 	"#d79921",
 	"#458588",
 	"#b16286",
 	"#689d6a",
-	"#7c6f64",
+	"#a89984",
 	"#928374",
-	"#9d0006",
-	"#79740e",
-	"#b57614",
-	"#076678",
-	"#8f3f71",
-	"#427b58",
-	"#3c3836",
+	"#fb4934",
+	"#b8bb26",
+	"#fabd2f",
+	"#83a598",
+	"#d3869b",
+	"#8ec07c",
+	"#ebdbb2",
 };
 
 
@@ -163,7 +146,7 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 15; // 7;
+unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
 static unsigned int defaultcs = 7;
 static unsigned int defaultrcs = 15;
