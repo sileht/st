65f7910de586e7e77b7b5e81603fd5f4661e0260
Mehdi Abaakouk <sileht@sileht.net>
2020-03-29 17:01:37 +0200

    change font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono Nerd Font:size=10:antialias=true:autohint=true";
+static char *font = "MesloLGS Nerd Font:size=10:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
